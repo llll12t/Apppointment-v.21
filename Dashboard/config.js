bc4dceb3cf32c8be843a721c385bf898ca2f63d6
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-03 16:41:03 +0700

    Update config.js

--- Dashboard/config.js
+++ Dashboard/config.js
@@ -1,5 +1,4 @@
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwRDs7takd9ozlconIGh3kVaBBzjMKP1Il_cOSVZ_RMkJNigputZPMFvA3GYtSi1QOCDA/exec';
 const CONFIG_URL = 'https://script.google.com/macros/s/AKfycbyCtMYBDIPQOOi4GGn2MuK2oL0Vbwg_o6MEa3IzpcXdkdyZbqlyCa0rUm4tsNcQvMIG/exec';
 
-const LIFF_ID = '2007432636-DpowZgEb'; // Booking
 const LIFF_ID_HIS = '2007432636-ROyNVwOA'; // History
